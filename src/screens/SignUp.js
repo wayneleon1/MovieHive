af5157a030eb15beaec48afc4dcb3c3ee3cf32b9
wayneleon1 <rurangwaleon10@gmail.com>
2024-06-03 10:32:27 +0200

    implement sigup using firebase

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -6,15 +6,92 @@ import {
   Image,
   TouchableOpacity,
   StatusBar,
+  Alert,
 } from "react-native";
 import Container from "../components/UI/Container";
 import { globalStyles } from "../styles/global";
 import { COLORS } from "../components/constraint";
 import { AntDesign } from "@expo/vector-icons";
 import { TextInput, Button } from "react-native-paper";
+import { FIREBASE_AUTH } from "../../Auth/Firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const SignUp = ({ navigation }) => {
   const [toggle, setToggle] = useState(false);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [comfrimPassword, setComfrimPassword] = useState("");
+
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [comfrimPasswordError, setComfrimPasswordError] = useState("");
+
+  // Basic email validation regex
+  const isValidEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const validForm = () => {
+    let valid = true;
+
+    if (!email.trim()) {
+      setEmailError("Email is required");
+      valid = false;
+    } else if (!isValidEmail(email)) {
+      setEmailError("Email provided is invalid ");
+      valid = false;
+    } else {
+      setEmailError("");
+    }
+    if (!password.trim()) {
+      setPasswordError("Password is invalid");
+      valid = false;
+    } else if (password.length < 8) {
+      setPasswordError("Password must be over 8 chars long ");
+      valid = false;
+    } else {
+      setPasswordError("");
+    }
+    if (!comfrimPassword.trim()) {
+      setComfrimPasswordError("Comfrim Password can't be empty");
+      valid = false;
+    } else if (comfrimPassword !== password) {
+      setComfrimPasswordError("Comfrim Password doesn't match with Password");
+      valid = false;
+    } else {
+      setComfrimPasswordError("");
+    }
+    return valid;
+  };
+
+  const handleSignIn = async () => {
+    if (validForm() === true) {
+      try {
+        const response = await createUserWithEmailAndPassword(
+          FIREBASE_AUTH,
+          email,
+          password
+        );
+        Alert.alert(
+          `Hello ${response.user.email}`,
+          "Account created successfully! Welcome aboard.",
+          [
+            {
+              text: "Continue",
+              onPress: () => navigation.navigate("SignIn"),
+            },
+          ]
+        );
+      } catch (error) {
+        Alert.alert("OOPS!", "Something went wrong!", [
+          { text: "OK", onPress: () => console.log("alert closed") },
+        ]);
+      }
+    }
+  };
+
   return (
     <Container>
       <StatusBar barStyle="light-content" backgroundColor="#26282C" />
@@ -74,7 +151,13 @@ const SignUp = ({ navigation }) => {
                 marginTop: 10,
                 backgroundColor: "#26282C",
               }}
+              value={email}
+              error={!!emailError}
+              onChangeText={(e) => setEmail(e)}
             />
+            {emailError ? (
+              <Text style={{ color: "crimson" }}>{emailError}</Text>
+            ) : null}
             <TextInput
               label="Password"
               secureTextEntry={!toggle}
@@ -90,7 +173,13 @@ const SignUp = ({ navigation }) => {
               }
               placeholderTextColor="white"
               style={{ marginTop: 20, backgroundColor: "#26282C" }}
+              value={password}
+              onChangeText={(p) => setPassword(p)}
+              error={!!passwordError}
             />
+            {passwordError ? (
+              <Text style={{ color: "crimson" }}>{passwordError}</Text>
+            ) : null}
             <TextInput
               label="Comfrim Password"
               secureTextEntry={!toggle}
@@ -110,12 +199,18 @@ const SignUp = ({ navigation }) => {
                 marginBottom: 30,
                 backgroundColor: "#26282C",
               }}
+              value={comfrimPassword}
+              onChangeText={(e) => setComfrimPassword(e)}
+              error={!!comfrimPasswordError}
             />
+            {comfrimPasswordError ? (
+              <Text style={{ color: "crimson" }}>{comfrimPasswordError}</Text>
+            ) : null}
             <Button
               mode="contained"
               buttonColor={COLORS.PRIMARY}
               textColor={COLORS.DARK}
-              onPress={() => navigation.navigate("SignIn")}
+              onPress={() => handleSignIn()}
               style={globalStyles.btn}
             >
               Sign Up
